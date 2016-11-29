f94ef29f5387a05542a2a2c1459de2c865177afd
Ender ÖZTURAN <eozturan@innova.com.tr>
2016-11-29 09:26:04 +0300

    JavaScript first code

---
+++ JavaScript2/script.js
@@ -0,0 +1,6 @@
+var userName = prompt("What is your name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("How old are you?");
+
+console.log("Your full name is " + userName+ " "+ lastName);
+console.log("Your age is "+ userAge);
